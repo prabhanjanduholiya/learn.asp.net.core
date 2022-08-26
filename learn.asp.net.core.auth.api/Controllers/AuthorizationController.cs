709e6442813e0c7f3f153ee0f2b420c96124eb46
Prabhanjan Duholiya <duholiya.prabhanjan@gmail.com>
2022-08-26 13:23:57 +0530

    sample poc

---
+++ learn.asp.net.core.auth.api/Controllers/AuthorizationController.cs
@@ -0,0 +1,28 @@
+﻿using learn.asp.net.core.auth.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learn.asp.net.core.auth.api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly ILogger<AuthorizationController> _logger;
+
+        public AuthorizationController(ILogger<AuthorizationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("token")]
+        public string Login(LoginRequest loginRequest)
+        {
+            return "";
+        }
+    }
+}
---
+++ learn.asp.net.core.client.api/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace learn.asp.net.core.client.api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
